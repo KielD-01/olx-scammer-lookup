3022cc9cf7be721355d8d58b15324b9cb77d7ece
Roman Kozin <romanko95.rk@gmail.com>
2018-06-04 21:22:23 +0300

    Updated hApiServer

--- server/index.js
+++ server/index.js
@@ -3,26 +3,26 @@ const hApi = require('hapi');
 const hApiServer = hApi.server(~~process.env.PORT || 3000, '0.0.0.0');
 
 hApiServer
-    .route({
-        method: 'GET',
-        path: '/',
-        handler(r, h) {
-            return {
-                dev: 'romanko95.rk@gmail.com'
+    .route([
+        {
+            method: 'GET',
+            path: '/',
+            handler(r, h) {
+                return {
+                    dev: 'romanko95.rk@gmail.com'
+                }
+            }
+        },
+        {
+            method: 'GET',
+            path: '/get-phones/{id}/{token}',
+            handler: function (r, h) {
+                const phonesUrl = `https://www.olx.ua/ajax/misc/contact/phone/${r.params.id}/?pt=${r.params.token}`;
+
+                return {phonesUrl};
             }
         }
-    });
-
-hApiServer
-    .route({
-        method: 'GET',
-        path: '/get-phones/{id}/{token}',
-        handler: function (r, h) {
-            const phonesUrl = `https://www.olx.ua/ajax/misc/contact/phone/${r.params.id}/?pt=${r.params.token}`;
-
-            return {phonesUrl};
-        }
-    });
+    ]);
 
 const init = async () => {
 
